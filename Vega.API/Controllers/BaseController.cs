fdec24f77ac7fc565b59b6eb97a09231cc50779b
Yasin Can <yasincan.cs@outlook.com>
2020-05-10 01:19:37 +0300

    Initial

---
+++ Vega.API/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vega.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseController : ControllerBase
+    {
+    }
+}
---
+++ Vega.API/Controllers/BooksController.cs
@@ -0,0 +1,72 @@
+using Vega.API.Models;
+using Vega.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Vega.API.Controllers
+{
+    public class BooksController : BaseController
+    {
+        private readonly BookService _bookService;
+
+        public BooksController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Book>> Get() =>
+            _bookService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetBook")]
+        public ActionResult<Book> Get(string id)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        [HttpPost]
+        public ActionResult<Book> Create(Book book)
+        {
+            _bookService.Create(book);
+
+            return CreatedAtRoute("GetBook", new { id = book.Id.ToString() }, book);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Book bookIn)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _bookService.Update(id, bookIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _bookService.Remove(book.Id);
+
+            return NoContent();
+        }
+    }
+}
